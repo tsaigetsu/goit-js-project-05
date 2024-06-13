f83233d6bf9e13048dd41da395d4a84b948e2dc4
Inkom007 <inkom0703@gmail.com>
2024-06-13 13:47:06 +0100

    MyProject/adaptive for tab and desk/nr3

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const projects = [
     { imageSmall: `./img/my-projects/project1(1x).jpg`, imageLarge: `./img/my-projects/project1(2x).jpg`, imageSmallMob: `./img/my-projects/project1mob(1x).jpg`, imageLargeMob: `./img/my-projects/project1mob(2x).jpg`, imageSmallTab: `./img/my-projects/project1tab(1x).jpg`, imageLargeTab: `./img/my-projects/project1tab(2x).jpg`, name: `wallet webservice`, link: `https://github.com/tsaigetsu/goit-js-project-05` },
     { imageSmall: `./img/my-projects/project2(1x).jpg`, imageLarge: `./img/my-projects/project2(2x).jpg`,imageSmallMob: `./img/my-projects/project2mob(1x).jpg`, imageLargeMob: `./img/my-projects/project2mob(2x).jpg`, imageSmallTab: `./img/my-projects/project2tab(1x).jpg`, imageLargeTab: `./img/my-projects/project2tab(2x).jpg`, name:`green harvest webservice`, link: `https://github.com/tsaigetsu/goit-js-project-05` },
-    { imageSmall: `./img/my-projects/project3(1x).jpg`, imageLarge: `./img/my-projects/project3(2x).jpg`, imageSmallMob: `./img/my-projects/project3mob(1x).jpg`, imageLargeMob: `./img/my-projects/project3mob(2x).jpg`, imageSmallTab: `./img/my-projects/project3tab(1x).jpg`, imageLargeTab: `./img/my-projects/project3tab(2x).jpg`, name:`English excellence webservice` , link: `https://github.com/tsaigetsu/goit-js-project-05` },
+    { imageSmall: `./img/my-projects/project3(1x).jpg`, imageLarge: `./img/my-projects/project3(2x).jpg`, imageSmallMob: `./img/my-projects/project3mob(1x).jpg`, imageLargeMob: `./img/my-projects/project3mob(2x).jpg`, imageSmallTab: `./img/my-projects/project3tab(1x).jpg`, imageLargeTab: `./img/my-projects/project3tab(2x).jpg`, name:`English excellence website` , link: `https://github.com/tsaigetsu/goit-js-project-05` },
     {imageSmall: `./img/my-projects/project4(1x).jpg`, imageLarge: `./img/my-projects/project4(2x).jpg`, imageSmallMob: `./img/my-projects/project4(1x).jpg`, imageLargeMob: `./img/my-projects/project4(2x).jpg`, imageSmallTab: `./img/my-projects/project4(1x).jpg`, imageLargeTab: `./img/my-projects/project4(2x).jpg`, name: `power pulse webservice`, link: `https://github.com/tsaigetsu/goit-js-project-05`},
     { imageSmall: `./img/my-projects/project5(1x).jpg`, imageLarge: `./img/my-projects/project5(2x).jpg`,imageSmallMob: `./img/my-projects/project5(1x).jpg`, imageLargeMob: `./img/my-projects/project5(2x).jpg`,imageSmallTab: `./img/my-projects/project5(1x).jpg`, imageLargeTab: `./img/my-projects/project5(2x).jpg`, name:`mimino website`, link: `https://github.com/tsaigetsu/goit-js-project-05`},
     { imageSmall: `./img/my-projects/project6(1x).jpg`, imageLarge: `./img/my-projects/project6(2x).jpg`,imageSmallMob: `./img/my-projects/project6(1x).jpg`, imageLargeMob: `./img/my-projects/project6(2x).jpg`, imageSmallTab: `./img/my-projects/project6(1x).jpg`, imageLargeTab: `./img/my-projects/project6(2x).jpg`,name:`vyshyvanka vibes Landing Page`, link: `https://github.com/tsaigetsu/goit-js-project-05`},
@@ -27,11 +27,13 @@ function projectsCreated(project) {
 <img class="myProjectImg" src="${project.imageSmall}" alt="${project.name}">
 </picture>
 <p class="project-tech">React, JavaScript, Node JS, Git</p>
+<div class="my-project-name-and-link-container">
 <h3 class="project-name">${project.name}</h3>
 <div class="my-project-link-container">
-<a class="my-project-link" href="${project.link}">Visit<svg class="icon-arrow" width="14.5" height=14.5">
+<a class="my-project-link" href="${project.link}">Visit<svg class="icon-arrow" width="24" height="24">
     <use href="./public/symbol-defs.svg#icon-greenArrow"></use>
   </svg></a>
+  </div>
   </div>
 </li>`
 };
