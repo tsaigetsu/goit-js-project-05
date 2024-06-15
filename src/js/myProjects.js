fc696deb26a1955b5d068b8143dcfa8daaab92a8
Inkom007 <inkom0703@gmail.com>
2024-06-15 01:07:58 +0100

    myProject/import Images / nr 6

--- src/js/myProjects.js
+++ src/js/myProjects.js
@@ -1,23 +1,46 @@
+import project1  from '/img/my-projects/project1(1x).jpg';
+import project1_2x from '/img/my-projects/project1(2x).jpg';
+import project2  from '/img/my-projects/project2(1x).jpg';
+import project2_2x from '/img/my-projects/project2(2x).jpg';
+import project3  from '/img/my-projects/project3(1x).jpg';
+import project3_2x from '/img/my-projects/project3(2x).jpg';
+import project4  from '/img/my-projects/project4(1x).jpg';
+import project4_2x from '/img/my-projects/project4(2x).jpg';
+import project5  from '/img/my-projects/project5(1x).jpg';
+import project5_2x from '/img/my-projects/project5(2x).jpg';
+import project6  from '/img/my-projects/project6(1x).jpg';
+import project6_2x from '/img/my-projects/project6(2x).jpg';
+import project7  from '/img/my-projects/project7(1x).jpg';
+import project7_2x from '/img/my-projects/project7(2x).jpg';
+import project8  from '/img/my-projects/project8(1x).jpg';
+import project8_2x from '/img/my-projects/project4(2x).jpg';
+import project9  from '/img/my-projects/project9(1x).jpg';
+import project9_2x from '/img/my-projects/project9(2x).jpg';
+import project10  from '/img/my-projects/project10(1x).jpg';
+import project10_2x from '/img/my-projects/project10(2x).jpg';
+
+
+
 const projects = [
-    { imageSmall: `./img/my-projects/project1(1x).jpg`, imageLarge: `./img/my-projects/project1(2x).jpg`, imageSmallMob: `./img/my-projects/project1mob(1x).jpg`, imageLargeMob: `./img/my-projects/project1mob(2x).jpg`, imageSmallTab: `./img/my-projects/project1tab(1x).jpg`, imageLargeTab: `./img/my-projects/project1tab(2x).jpg`, name: `wallet webservice`, link: `https://github.com/tsaigetsu/goit-js-project-05` },
-    { imageSmall: `./img/my-projects/project2(1x).jpg`, imageLarge: `./img/my-projects/project2(2x).jpg`,imageSmallMob: `./img/my-projects/project2mob(1x).jpg`, imageLargeMob: `./img/my-projects/project2mob(2x).jpg`, imageSmallTab: `./img/my-projects/project2tab(1x).jpg`, imageLargeTab: `./img/my-projects/project2tab(2x).jpg`, name:`green harvest webservice`, link: `https://github.com/tsaigetsu/goit-js-project-05` },
-    { imageSmall: `./img/my-projects/project3(1x).jpg`, imageLarge: `./img/my-projects/project3(2x).jpg`, imageSmallMob: `./img/my-projects/project3mob(1x).jpg`, imageLargeMob: `./img/my-projects/project3mob(2x).jpg`, imageSmallTab: `./img/my-projects/project3tab(1x).jpg`, imageLargeTab: `./img/my-projects/project3tab(2x).jpg`, name:`English excellence website` , link: `https://github.com/tsaigetsu/goit-js-project-05` },
-    {imageSmall: `./img/my-projects/project4(1x).jpg`, imageLarge: `./img/my-projects/project4(2x).jpg`, imageSmallMob: `./img/my-projects/project4(1x).jpg`, imageLargeMob: `./img/my-projects/project4(2x).jpg`, imageSmallTab: `./img/my-projects/project4(1x).jpg`, imageLargeTab: `./img/my-projects/project4(2x).jpg`, name: `power pulse webservice`, link: `https://github.com/tsaigetsu/goit-js-project-05`},
-    { imageSmall: `./img/my-projects/project5(1x).jpg`, imageLarge: `./img/my-projects/project5(2x).jpg`,imageSmallMob: `./img/my-projects/project5(1x).jpg`, imageLargeMob: `./img/my-projects/project5(2x).jpg`,imageSmallTab: `./img/my-projects/project5(1x).jpg`, imageLargeTab: `./img/my-projects/project5(2x).jpg`, name:`mimino website`, link: `https://github.com/tsaigetsu/goit-js-project-05`},
-    { imageSmall: `./img/my-projects/project6(1x).jpg`, imageLarge: `./img/my-projects/project6(2x).jpg`,imageSmallMob: `./img/my-projects/project6(1x).jpg`, imageLargeMob: `./img/my-projects/project6(2x).jpg`, imageSmallTab: `./img/my-projects/project6(1x).jpg`, imageLargeTab: `./img/my-projects/project6(2x).jpg`,name:`vyshyvanka vibes Landing Page`, link: `https://github.com/tsaigetsu/goit-js-project-05`},
-    { imageSmall: `./img/my-projects/project7(1x).jpg`, imageLarge: `./img/my-projects/project7(2x).jpg`,imageSmallMob: `./img/my-projects/project7(1x).jpg`, imageLargeMob: `./img/my-projects/project7(2x).jpg`,imageSmallTab: `./img/my-projects/project7(1x).jpg`, imageLargeTab: `./img/my-projects/project7(2x).jpg`, name:`chego jewelry website`, link: `https://github.com/tsaigetsu/goit-js-project-05` },
-    { imageSmall: `./img/my-projects/project8(1x).jpg`, imageLarge: `./img/my-projects/project8(2x).jpg`,imageSmallMob: `./img/my-projects/project8(1x).jpg`, imageLargeMob: `./img/my-projects/project8(2x).jpg`, imageSmallTab: `./img/my-projects/project8(1x).jpg`, imageLargeTab: `./img/my-projects/project8(2x).jpg`,name:`energy flow webservice`, link: `https://github.com/tsaigetsu/goit-js-project-05` },
-    { imageSmall: `./img/my-projects/project9(1x).jpg`, imageLarge: `./img/my-projects/project9(2x).jpg`,imageSmallMob: `./img/my-projects/project9(1x).jpg`, imageLargeMob: `./img/my-projects/project9(2x).jpg`,imageSmallTab: `./img/my-projects/project9(1x).jpg`, imageLargeTab: `./img/my-projects/project9(2x).jpg`,name:`fruitbox online store`, link: `https://github.com/tsaigetsu/goit-js-project-05` },
-    { imageSmall: `./img/my-projects/project10(1x).jpg`, imageLarge: `./img/my-projects/project10(2x).jpg`,imageSmallMob: `./img/my-projects/project10(1x).jpg`, imageLargeMob: `./img/my-projects/project10(2x).jpg`, imageSmallTab: `./img/my-projects/project10(1x).jpg`, imageLargeTab: `./img/my-projects/project10(2x).jpg`,name:`starlight studio landing page`, link: `https://github.com/tsaigetsu/goit-js-project-05` },
+    { name: `wallet webservice`,imageSmall: project1, imageLarge: project1_2x, link: `https://github.com/tsaigetsu/goit-js-project-05` },
+    { name:`green harvest webservice`,imageSmall: project2, imageLarge: project2_2x, link: `https://github.com/tsaigetsu/goit-js-project-05` },
+    { name:`English excellence website` ,imageSmall: project3, imageLarge: project3_2x,  link: `https://github.com/tsaigetsu/goit-js-project-05` },
+    { name: `power pulse webservice`, imageSmall: project4, imageLarge: project4_2x, link: `https://github.com/tsaigetsu/goit-js-project-05`},
+    { name:`mimino website`, imageSmall: project5, imageLarge: project5_2x,link: `https://github.com/tsaigetsu/goit-js-project-05`},
+    { name:`vyshyvanka vibes Landing Page`,imageSmall: project6, imageLarge: project6_2x, link: `https://github.com/tsaigetsu/goit-js-project-05`},
+    { name:`chego jewelry website`, imageSmall: project7, imageLarge: project7_2x, link: `https://github.com/tsaigetsu/goit-js-project-05` },
+    { name:`energy flow webservice`,imageSmall: project8, imageLarge: project8_2x, link: `https://github.com/tsaigetsu/goit-js-project-05` },
+    { name:`fruitbox online store`, imageSmall: project9, imageLarge: project9_2x,link: `https://github.com/tsaigetsu/goit-js-project-05` },
+    { name:`starlight studio landing page`, imageSmall: project10, imageLarge: project10_2x,link: `https://github.com/tsaigetsu/goit-js-project-05` },
 ];
 
-const projectsList = document.querySelector(".projects-list");
-const btnLoadMore = document.querySelector(".load-more-btn");
+const projectsList = document.querySelector('.projects-list');
+const btnLoadMore = document.querySelector('.load-more-btn');
 let currentIndex = 0;
 const projectLoad = 3;
 
 
-export function projectsCreated(project) {
+function projectsCreated(project) {
     return `<li>
     <picture>
 <source srcset="${project.imageSmall} 1x, ${project.imageLarge} 2x" media="(min-width: 1280px)">
@@ -37,7 +60,7 @@ export function projectsCreated(project) {
 </li>`
 };
 
- export function projectsTemplate(arr, startIndex, count) {
+ function projectsTemplate(arr, startIndex, count) {
      return arr.slice(startIndex, startIndex + count).map(projectsCreated).join("");
 }
    
