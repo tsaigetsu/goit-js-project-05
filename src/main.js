64c4416dbe8586f1448e5b7b0393dbf1f23da4d5
AliaNord <alia.nord.adventurer@gmail.com>
2024-06-17 14:21:23 +0200

    reviews css

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { darkMode } from './js/darkMode';
 import { mobileMenu } from './js/mobileMenu';
 import { loadMore } from './js/myProjects';
-import initializeAccordion from './js/faq.js';
+import initializeAccordion from './js/faq';
 import './js/reviews';
 
 darkMode();
