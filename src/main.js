746c0a2093ba6c96e0e86b007941ec5531b3b3dd
voytkoo <avoytko86@gmail.com>
2024-06-17 14:14:49 +0300

    workTogether.update.css.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { darkMode } from './js/darkMode'
 import { mobileMenu } from './js/mobileMenu'
-
+import { workTogether } from './js/workTogether'
 
 import { loadMore } from './js/myProjects'
 
 darkMode()
 mobileMenu()
 loadMore()
+workTogether()
