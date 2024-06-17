af12f41936cf8becb2e209012528c10d1c4be3cb
voytkoo <avoytko86@gmail.com>
2024-06-17 14:34:55 +0300

    workTogether.update.modal.css.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import { darkMode } from './js/darkMode'
-import { mobileMenu } from './js/mobileMenu'
-
-
-import { loadMore } from './js/myProjects'
-
-darkMode()
-mobileMenu()
-loadMore()
+import { darkMode } from './js/darkMode';
+import { mobileMenu } from './js/mobileMenu';
+import { loadMore } from './js/myProjects';
+import initializeAccordion from './js/faq.js';
+import './js/reviews';
+import { workTogether } from './js/workTogether'
+darkMode();
+mobileMenu();
+loadMore();
+initializeAccordion();
+workTogether()
