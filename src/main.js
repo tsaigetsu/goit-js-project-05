1afab174318dcb4ba5ac02eb5d3c233ae789ed12
AliaNord <alia.nord.adventurer@gmail.com>
2024-06-18 12:30:47 +0200

    main.js & workTogether html

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import { mobileMenu } from './js/mobileMenu';
 import { loadMore } from './js/myProjects';
 import initializeAccordion from './js/faq';
 import './js/reviews';
-import { workTogether } from './js/workTogether'
+import { workTogether } from './js/workTogether';
+
 darkMode();
 mobileMenu();
 loadMore();
 initializeAccordion();
-AOS.init();
-workTogether()
+AOS.init({
+  once: true,
+});
+workTogether();
